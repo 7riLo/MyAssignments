68aaed18f6c3b9618febe12b320f5623bbe29681
7riLo <lwyant@mail.bradley.edu>
2020-09-27 22:15:55 -0500

    part 1 complete
    score is now registering and part 1 follow along complete.

---
+++ Prototype3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //save the starting position of the background to a vector3 variable
+        startPosition = transform.position;
+
+        //set the repeatWidth to half of the width of the background using BoxCollider
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if background is farther to the left than repeat width, reset to start position
+        if(transform.position.x < startPosition.x - repeatWidth)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
---
+++ Prototype3/Assets/Scripts/TriggerZoneAddScore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TriggerZoneAddScore : MonoBehaviour
+{
+
+    private UIManager uIManager;
+
+    private bool triggered = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIManager = GameObject.FindObjectOfType<UIManager>();   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !triggered)
+        {
+
+            triggered = true;
+            //would not work with lowercare uIManager variable, so this is a work around.
+            UIManager.score++;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
